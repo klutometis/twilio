5f0fb4d25068e9e2b1099c0f7d2aa21ea89ea2ac
Peter Danenberg <pcd@roxygen.org>
2013-08-17 23:15:14 -0500

    Some basic parameters

--- twilio-core.scm
+++ twilio-core.scm
@@ -4,3 +4,15 @@
 @(email "pcd@roxygen.org")
 @(username "klutometis")
 @(noop)
+
+(define twilio-sid
+  (make-parameter (get-environment-variable "TWILIO_SID")))
+
+(define twilio-auth
+  (make-parameter (get-environment-variable "TWILIO_AUTH")))
+
+(define twilio-from
+  (make-parameter (get-environment-variable "TWILIO_FROM")))
+
+(define twilio-url
+  (make-parameter "https://~a:~a@api.twilio.com/2010-04-01/Accounts/~a/~a"))
