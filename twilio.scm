3cd30cf453752378b1d8038566832815332515df
Peter Danenberg <pcd@roxygen.org>
2013-08-18 23:30:30 -0700

    Module

--- twilio.scm
+++ twilio.scm
@@ -1,6 +1,20 @@
 (module twilio
-  ()
+  (twilio-sid
+   twilio-auth
+   twilio-from
+   twilio-url
+   twilio-make-call
+   twilio-send-sms)
 
-  (import chicken scheme)
+  (import chicken
+          data-structures
+          scheme)
+
+  (use debug
+       extras
+       http-client
+       matchable
+       s
+       srfi-1)
 
   (include "twilio-core.scm"))
