746a0d75d4c6d7e56b4f51a5f5e8b873c7a1dfeb
Peter Danenberg <pcd@roxygen.org>
2013-08-31 09:17:27 -0500

    Test without parameters.

--- tests/run.scm
+++ tests/run.scm
@@ -1,10 +1,18 @@
 (use test
      twilio)
 
-(test "Twilio write, response, say"
+(test "Twilio write, response, say with parameters"
       "<Response><Say voice=\"alice\" loop=\"2\" language=\"de\">harro</Say></Response>"
       (with-output-to-string
         (lambda ()
           (twilio-write
            (twilio-response
             (twilio-say "harro" voice: "alice" loop: "2" language: "de"))))))
+
+(test "Twilio write, response, SMS with no parameters"
+      "<Response><Sms>harro</Sms></Response>"
+      (with-output-to-string
+        (lambda ()
+          (twilio-write
+           (twilio-response
+            (twilio-sms "harro"))))))
